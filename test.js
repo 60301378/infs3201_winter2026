064fe3e9b9541baec264f4073a16ee04c5df5dde
60301378 <60301378@udst.edu.qa>
2026-02-11 17:41:34 +0300

    Update test.js

--- test.js
+++ test.js
@@ -2,6 +2,7 @@ const business = require('./business')
 
 async function test() {
     let r = await business.updateCapacity('INFS2201', 3)
+    let z = await business.updateCapacity('INFS2201', 3)
     console.log(r)
 }
     
